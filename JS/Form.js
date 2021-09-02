2b46594a9fa32fd7caa2db9194337293088d315a
Rishabh-888 <74863495+Rishabh-888@users.noreply.github.com>
2021-09-02 18:23:09 +0530

    Add files via upload

---
+++ JS/Form.js
@@ -0,0 +1,25 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement('h1');
+        title.html("CAR RACING GAME");
+        title.position(130, 0);
+        var input = createInput("NAME");
+        input.position(130, 160);
+        var button = createButton("PLAY");
+        button.position(250, 200);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount+=1;
+            player.update(name)
+            player.updateCount(playerCount);
+            var greeting = createElement('h2');
+            greeting.html("Hello"+name);
+            greeting.position(130, 160);
+        })
+    }
+}
